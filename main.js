4bdce0f99364f698434bea705277a58335cf27f4
MatteoZattini <matteozattini@hotmail.com>
2024-04-28 17:12:16 +0200

    m3 con template literal

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ while ( i < lista.length ) {
     i++;
     const element = lista[i]
     console.log(i, element)
+// append
     const liElement = document.createElement("li")
     liElement.innerText = element;
     listaElelement.append(liElement);
+// template literal
+    listaElelement.innerHTML += `<li>${element}</li>`
 }
