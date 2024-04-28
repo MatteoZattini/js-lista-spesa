8f889c7509133520cf974230d0ccfd10a6b7e6db
MatteoZattini <matteozattini@hotmail.com>
2024-04-28 16:32:13 +0200

    m1 ciclo for

---
+++ main.js
@@ -0,0 +1,13 @@
+const lista = [
+    'pasta',
+    'pesto',
+    'pomodori',
+    'insalata',
+    'mirtilli',
+]
+
+console.log(lista)
+
+for (let i = 0; i < lista.length; i++) {
+    console.log(i)
+}
